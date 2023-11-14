4456fb25a26304f75ed6454abd93cca059c39d64
Noah <noah.hoogenboom@xs4all.nl>
2023-11-14 12:12:20 +0100

    test files for snellius

---
+++ test.m
@@ -0,0 +1,10 @@
+x = linspace(0, 2*pi, 100);
+y = sin(x);
+
+plot(x, y)
+
+title('Sine Wave')
+xlabel('x-axis')
+ylabel('y-axis')
+
+grid on
--- test_tifffile.py
+++ test_tifffile.py
@@ -1,8 +1,11 @@
-import tifffile
+import imageio
 import matplotlib.pyplot as plt
 
-with tifffile.TiffFile("H21-066.1_HE331_0005_Scan1.qptiff") as tif:
-    image = tif.asarray()
+# Replace 'your_file.qptiff' with the path to your QPTiff file
+qptiff_data = imageio.imread("H21-066.4_HE332_105_Scan1.qptiff")
 
-plt.imshow(image)
+# Display the image using matplotlib
+plt.imshow(qptiff_data, cmap="gray")  # You can adjust the colormap as needed
+plt.title("QPTiff Image")
+plt.colorbar()  # Add a colorbar for reference if necessary
 plt.show()
